4643a20a69a7532ea00126c7c1d064706cf49948
JelmerKorten <jelmer.korten@gmail.com>
2023-06-11 17:18:44 +0400

    add license

--- setupproj.py
+++ setupproj.py
@@ -1,5 +1,24 @@
+    # SetupProj plots notams on a streetmap.
+    # Copyright (C) 2023  Jelmer Korten (korty.codes)
+
+    # SetupProj is free software: you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation, either version 3 of the License, or
+    # (at your option) any later version.
+
+    # SetupProj is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU General Public License for more details.
+
+    # You should have received a copy of the GNU General Public License
+    # along with SetupProj.  If not, see <https://www.gnu.org/licenses/>.
+
+    # Contact: korty.codes@gmail.com
+
 # setupproj
 # To set up a project
+# version 1.0.1
 
 import sys
 from os import system, mkdir, makedirs, chdir
